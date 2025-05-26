b1fe3065c34cbdf0b3b201b71f5f3a7841567542
BayryamB <bayryambashchoban@gmail.com>
2025-05-26 15:48:32 +0300

    05 & 06 completed

---
+++ advanced data types/06combinedFunction.ts
@@ -0,0 +1,33 @@
+let names = {
+    fName: "John",
+    lName: "Doe",
+    age: 22,
+    getPersonInfo() {
+        return `${this.fName} ${this.lName}, age ${this.age}`;
+    },
+};
+
+let location1 = {
+    city: "Boston",
+    street: "Nowhere street",
+    number: 13,
+    postalCode: 51225,
+    getAddressInfo() {
+        return `${this.street} ${this.number}, ${this.city} ${this.postalCode}`;
+    },
+};
+
+type namesType = typeof names;
+type locationType = typeof location1;
+
+function createCombinedFunction(names: namesType, location: locationType) {
+    return function (combinedObject: namesType & locationType) {
+        console.log(
+            `Hello, ${combinedObject.getPersonInfo()} from ${combinedObject.getAddressInfo()}`
+        );
+    };
+}
+
+let combinedFunction = createCombinedFunction(names, location1);
+let combinedPerson = Object.assign({}, names, location1);
+combinedFunction(combinedPerson);
---
+++ advanced data types/out/06combinedFunction.js
@@ -0,0 +1,27 @@
+"use strict";
+let names = {
+    fName: "John",
+    lName: "Doe",
+    age: 22,
+    getPersonInfo() {
+        return `${this.fName} ${this.lName}, age ${this.age}`;
+    },
+};
+let location1 = {
+    city: "Boston",
+    street: "Nowhere street",
+    number: 13,
+    postalCode: 51225,
+    getAddressInfo() {
+        return `${this.street} ${this.number}, ${this.city} ${this.postalCode}`;
+    },
+};
+function createCombinedFunction(names, location) {
+    return function (combinedObject) {
+        console.log(`Hello, ${combinedObject.getPersonInfo()} from ${combinedObject.getAddressInfo()}`);
+    };
+}
+let combinedFunction = createCombinedFunction(names, location1);
+let combinedPerson = Object.assign({}, names, location1);
+combinedFunction(combinedPerson);
+//# sourceMappingURL=06combinedFunction.js.map
