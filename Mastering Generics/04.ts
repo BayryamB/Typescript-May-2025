dad40ee00ec42fac6b86be05b9acb0dffef1c079
BayryamB <bayryambashchoban@gmail.com>
2025-06-10 14:00:53 +0300

    03 & 04 from generics done

---
+++ Mastering Generics/04.ts
@@ -0,0 +1,18 @@
+type inputParam<T> = T extends number ? number : string;
+
+function conditionalNumber<T>(param: inputParam<T>) {
+    if (typeof param === "number") {
+        console.log(param.toFixed(2));
+    } else {
+        console.log(param);
+    }
+}
+
+conditionalNumber<number>(20.3555);
+
+conditionalNumber<string>("wow");
+
+conditionalNumber<boolean>("a string");
+// conditionalNumber<boolean>(30);
+
+// conditionalNumber<number>('test');
